88c5ec2a6e9e773dab70304419ab497b578c49e7
Ryou Ohsawa <xr0038@gmail.com>
2020-11-24 02:06:06 +0900

    client: command-line arguments preliminary defined

--- sidex/client.py
+++ sidex/client.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, re, logging, requests
+
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser as ap
+  parser = ap(description='sidex minimal client')
+  parser.add_argument('url', type=str,
+    help='address to SIDEX server')
+  parser.add_argument(
+    '--get-token', dest='get_token', metavar='token', type=str,
+    help='limit get function by setting token')
+  parser.add_argument(
+    '--put-token', dest='put_token', metavar='token', type=str,
+    help='enable put function by setting token')
+  parser.add_argument(
+    '--delete-token', dest='delete_token', metavar='token', type=str,
+    help='enable delete function by setting token.')
+
+  args = parser.parse_args()
+
+  payload =
