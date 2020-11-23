8a4cc90ac7a558c8cc515178a9c3b044afcec3ac
Ryou Ohsawa <xr0038@gmail.com>
2020-11-24 02:03:03 +0900

    server: set logger

--- sidex/server.py
+++ sidex/server.py
@@ -3,3 +3,16 @@
 from flask import Flask, Response
 from flask import request, url_for, render_template
 import os, re, logging, requests
+
+
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(logging.Formatter(
+  fmt='%(asctime)s %(levelname)s:%(name)s:%(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S'))
+
+werkzeug = logging.getLogger('werkzeug')
+werkzeug.setLevel('ERROR')
+werkzeug.handlers = [log_handler,]
+
+app = Flask(__name__)
+app.logger.handlers = [log_handler,]
