bebdb77aae78ac0cf07e1c013950a50185faba2b
Ryou Ohsawa <xr0038@gmail.com>
2020-11-24 23:57:40 +0900

    bugfix in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from glob import glob
-from setuptools import setup, Extension
+from setuptools import setup, find_packages, Extension
 import os,sys,re
 
 
 with open('README.md', 'r') as fd:
-  version = '0.1'
+  version = '0.2'
   author = 'Ryou Ohsawa'
   email = 'ohsawa@ioa.s.u-tokyo.ac.jp'
   description = 'SIDEX: Simple Data Exchange server over HTTP'
@@ -38,5 +38,6 @@ if __name__ == '__main__':
     long_description_content_type='text/markdown',
     url='https://bitbucket.org/ryou_ohsawa/sidex/src/master/',
     license=license,
+    packages=find_packages(),
     classifiers=classifiers,
     install_requires=['flask','requests',])
