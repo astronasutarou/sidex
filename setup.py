f25539ac5a11ded2e307ff5e9c98110819bd5b3f
Ryou Ohsawa <xr0038@gmail.com>
2021-01-28 05:29:31 +0900

    increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os,sys,re
 
 
 with open('README.md', 'r') as fd:
-  version = '0.2'
+  version = '0.3'
   author = 'Ryou Ohsawa'
   email = 'ohsawa@ioa.s.u-tokyo.ac.jp'
   description = 'SIDEX: Simple Data Exchange server over HTTP'
